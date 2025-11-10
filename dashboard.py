1fab295f0b4cfd6b756d901b83f9670cfc6187b0
rocket6317 <108830817+rocket6317@users.noreply.github.com>
2025-11-10 23:50:04 +0000

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,6 @@
 import os
 from datetime import timedelta
-from cache import get_cached_url
+from cache import peek_cached_entry
 
 BASE_URL = os.getenv("BASE_URL", "https://example.com")
 REFRESH_INTERVAL = int(os.getenv("REFRESH_INTERVAL", "21300"))
